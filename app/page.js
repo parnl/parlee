6937948b3ae38481346035ee88d480ff9fd0ca74
parnl <wn.losnikau@gmail.com>
2025-12-18 08:56:25 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1 style={{ fontSize: 40, marginBottom: 8 }}>Parlee</h1>
+      <p style={{ fontSize: 18, opacity: 0.8 }}>
+        MVP is deploying. Next: Today screen.
+      </p>
+    </main>
+  );
+}
